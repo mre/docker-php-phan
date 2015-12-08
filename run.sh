96f873bc7224f41a555fdf192ed330f868bb501a
Matthias Endler <matthias.endler@trivago.com>
2015-12-08 23:21:37 +0100

    Use find command to detect source files

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 # This is a handy trick to get a list of all php files in the current directory.
 # From http://www.lornajane.net/posts/2015/generating-a-file-list-for-phan
-grep -i -R -l "<?php" /code > filelist.txt
+find /code/src -name '*.php' > filelist.txt
 
 # Analyze the files with phan
 phan -f filelist.txt
